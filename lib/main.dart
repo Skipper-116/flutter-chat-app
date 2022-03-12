44d26d76c1af9f8e8ba6740032bf6e2a257ae33c
Roy Chanunkha <roychanunkha@gmail.com>
2022-03-12 10:58:13 +0200

    added a text editing controller to manage interactions with the text field

--- lib/main.dart
+++ lib/main.dart
@@ -20,9 +20,15 @@ class FriendlyChatApp extends StatelessWidget {
   }
 }
 
-class ChatScreen extends StatelessWidget {
+class ChatScreen extends StatefulWidget {
   const ChatScreen({Key? key}) : super(key: key);
 
+  @override
+  State<ChatScreen> createState() => _ChatScreenState();
+}
+
+class _ChatScreenState extends State<ChatScreen> {
+  final _textController = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
